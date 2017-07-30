064b35aaea99c0e475a00f818433f74f1f3cdbe9
Notmoo <skraig.boeuf@gmail.com>
2017-07-30 17:45:45 +0200

    Module Server, clsss ISaleDao : suppr d'un TODO (fait)

--- Workspace/server/src/main/WEB-INF/classes/com/pqt/server/module/sale/ISaleDao.java
+++ Workspace/server/src/main/WEB-INF/classes/com/pqt/server/module/sale/ISaleDao.java
@@ -3,7 +3,6 @@ package com.pqt.server.module.sale;
 import com.pqt.core.entities.sale.Sale;
 
 //TODO écrire Javadoc
-//TODO Créer implémentation
 public interface ISaleDao {
 
 	long submitSale(Sale sale);
