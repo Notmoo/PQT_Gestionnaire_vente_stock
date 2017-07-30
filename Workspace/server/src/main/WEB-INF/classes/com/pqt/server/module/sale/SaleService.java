ee38eb526388a51a2123aa0abb91585365755d94
Notmoo <skraig.boeuf@gmail.com>
2017-07-30 22:20:32 +0200

    Module Server : ajout de listeners à SaleService; Ajout d'un package contenant les clss et intrfc dédiées aux listeners du service

--- Workspace/server/src/main/WEB-INF/classes/com/pqt/server/module/sale/SaleService.java
+++ Workspace/server/src/main/WEB-INF/classes/com/pqt/server/module/sale/SaleService.java
@@ -2,6 +2,9 @@ package com.pqt.server.module.sale;
 
 import com.pqt.core.entities.sale.Sale;
 import com.pqt.server.exception.ServerQueryException;
+import com.pqt.server.module.sale.listeners.ISaleFirerer;
+import com.pqt.server.module.sale.listeners.ISaleListener;
+import com.pqt.server.module.sale.listeners.SimpleSaleFirerer;
 import com.pqt.server.module.stock.StockService;
 
 //TODO écrire Javadoc
@@ -9,17 +12,28 @@ import com.pqt.server.module.stock.StockService;
 public class SaleService {
 
     private ISaleDao dao;
+    private ISaleFirerer eventFirerer;
 
     public SaleService(StockService stockService) {
         dao = new NoRevertFileSaleDao(stockService);
+        eventFirerer = new SimpleSaleFirerer();
     }
 
     public long submitSale(Sale sale) throws ServerQueryException {
-		return dao.submitSale(sale);
+        long id = dao.submitSale(sale);
+        if(id!=-1) eventFirerer.fireSaleValidatedEvent(sale);
+		return id;
 	}
 
 	public void submitSaleRevert(long id) throws ServerQueryException {
         dao.submitSaleRevert(id);
 	}
 
+    public void addListener(ISaleListener l) {
+        eventFirerer.addListener(l);
+    }
+
+    public void removeListener(ISaleListener l){
+        eventFirerer.addListener(l);
+    }
 }
---
+++ Workspace/server/src/main/WEB-INF/classes/com/pqt/server/module/sale/listeners/SaleListenerAdapter.java
@@ -0,0 +1,10 @@
+package com.pqt.server.module.sale.listeners;
+
+import com.pqt.core.entities.sale.Sale;
+
+public class SaleListenerAdapter implements ISaleListener {
+    @Override
+    public void onSaleValidatedEvent(Sale sale) {
+
+    }
+}
