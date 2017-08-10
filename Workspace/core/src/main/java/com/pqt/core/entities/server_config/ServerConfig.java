0ceb0d72cda5467e884e85d16a60ab6c95170a70
Notmoo <skraig.boeuf@gmail.com>
2017-08-10 09:25:55 +0200

    Module Core : ajout clss config serveur, update enum MessageType & AccountLevel

---
+++ Workspace/core/src/main/java/com/pqt/core/entities/server_config/ServerConfig.java
@@ -0,0 +1,53 @@
+package com.pqt.core.entities.server_config;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerConfig {
+
+    private Map<ConfigFields, Boolean> fields;
+
+    public ServerConfig() {
+    }
+
+    public ServerConfig(Map<ConfigFields, Boolean> fields) {
+        this.fields = fields;
+    }
+
+    public ServerConfig(ConfigFields... configFields) {
+        fields = new HashMap<>();
+        Arrays.stream(configFields).forEach(field->fields.put(field, true));
+
+        EnumSet.allOf(ConfigFields.class).stream().filter(field->!fields.containsKey(field)).forEach(field->fields.put(field, false));
+    }
+
+    public Map<ConfigFields, Boolean> getFields() {
+        return fields;
+    }
+
+    public void setFields(Map<ConfigFields, Boolean> fields) {
+        this.fields = fields;
+    }
+
+    public boolean isSupported(ConfigFields field){
+        return fields.containsKey(field) && fields.get(field);
+    }
+
+    public void switchFieldValue(ConfigFields field){
+        if(fields.containsKey(field)){
+            fields.replace(field, !fields.get(field));
+        }else{
+            fields.put(field, true);
+        }
+    }
+
+    public boolean add(ConfigFields field, boolean value){
+        if(!fields.containsKey(field)){
+            fields.put(field, value);
+            return true;
+        }
+        return false;
+    }
+}
--- Workspace/core/src/main/java/com/pqt/core/entities/user_account/AccountLevel.java
+++ Workspace/core/src/main/java/com/pqt/core/entities/user_account/AccountLevel.java
@@ -1,8 +1,21 @@
 package com.pqt.core.entities.user_account;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 /**
  * Created by Notmoo on 18/07/2017.
  */
 public enum AccountLevel {
-    GUEST, STAFF, WAITER, MASTER
+    LOWEST, GUEST, STAFF, WAITER, MASTER;
+
+    public static AccountLevel getLowest(){
+        return Arrays.stream(AccountLevel.values()).sorted(Comparator.naturalOrder()).findFirst().orElse(null);
+    }
+
+    public static AccountLevel getHighest(){
+        return Arrays.stream(AccountLevel.values()).sorted(Comparator.reverseOrder()).findFirst().orElse(null);
+    }
 }
