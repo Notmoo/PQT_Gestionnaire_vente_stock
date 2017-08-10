7de32786ab62f396da281d39686ed1250e787e9e
Notmoo <skraig.boeuf@gmail.com>
2017-08-10 10:21:50 +0200

    Module Server, clss ServerQueryException : ajout constructeurs

--- Workspace/server/src/main/WEB-INF/classes/com/pqt/server/exception/ServerQueryException.java
+++ Workspace/server/src/main/WEB-INF/classes/com/pqt/server/exception/ServerQueryException.java
@@ -1,4 +1,19 @@
 package com.pqt.server.exception;
 
 public class ServerQueryException extends Exception {
+
+    public ServerQueryException() {
+    }
+
+    public ServerQueryException(String message) {
+        super(message);
+    }
+
+    public ServerQueryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServerQueryException(Throwable cause) {
+        super(cause);
+    }
 }
