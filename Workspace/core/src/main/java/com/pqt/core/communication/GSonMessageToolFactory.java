599ebae0192c01779346f977b2b3a57a964126d3
Notmoo-PC\Notmoo <guillaumeprost39@gmail.com>
2017-08-23 13:21:58 +0200

    Module Core, clss GSonMEssageToolFactory : suppr des imports inutilisés

--- Workspace/core/src/main/java/com/pqt/core/communication/GSonMessageToolFactory.java
+++ Workspace/core/src/main/java/com/pqt/core/communication/GSonMessageToolFactory.java
@@ -3,13 +3,6 @@ package com.pqt.core.communication;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.pqt.core.entities.members.PqtMember;
-import com.pqt.core.entities.members.PqtMemberType;
-import com.pqt.core.entities.messages.Message;
-import com.pqt.core.entities.messages.MessageType;
-import com.pqt.core.entities.product.Product;
-import com.pqt.core.entities.product.ProductUpdate;
-import com.pqt.core.entities.sale.Sale;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
