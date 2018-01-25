2123843247bd4246407ab23656dc0bdfcff2375e
Notmoo-PC\Notmoo <guillaumeprost39@gmail.com>
2018-01-25 17:45:23 +0100

    [CLIENT] #6.A : Supression du module log

--- Workspace/client/src/main/java/com/pqt/client/module/log/ILogLineMaker.java
+++
@@ -1,8 +0,0 @@
-package com.pqt.client.module.log;
-
-import com.pqt.core.entities.log.LogLine;
-import com.pqt.core.entities.log.ILoggable;
-
-public interface ILogLineMaker {
-	LogLine make(ILoggable loggable);
-}
--- Workspace/client/src/main/java/com/pqt/client/module/log/listeners/ILogListener.java
+++
@@ -1,9 +0,0 @@
-package com.pqt.client.module.log.listeners;
-
-import java.util.EventListener;
-
-public interface ILogListener extends EventListener {
-
-	public abstract void onLogEvent();
-
-}
